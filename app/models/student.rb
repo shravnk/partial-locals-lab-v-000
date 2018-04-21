ebb63f6f77d6f45c7dac9a738dd50ace57ef342e
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-21 20:55:05 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    self.all.select{|student| student.name == name}
+  end
 end
